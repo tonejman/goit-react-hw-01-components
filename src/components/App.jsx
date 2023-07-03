3ccf6dc61ce322636f6bec70c98540f8b347dbe4
tonejman <nej.man@interia.pl>
2023-07-03 11:29:24 +0200

    added components import and data paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import { Profile } from './Profile/Profile.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FriendList } from './FriendList/FriendList.jsx';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
+
+import user from '../path/user';
+import data from '../path/data';
+import friends from '../path/friends';
+import transactions from '../path/transactions';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
